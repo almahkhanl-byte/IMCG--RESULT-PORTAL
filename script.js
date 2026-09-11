2d90bc29e240e622190a753b29332d98d340242f
almahkhanl-byte <almahkhanalmah@gmail.com>
2026-09-11 11:53:24 +0500

    Create script.js for loading student data
    Add script.js to load student data from Google Sheets

---
+++ script.js
@@ -0,0 +1,20 @@
+const SHEET_URL = "https://docs.google.com/spreadsheets/d/117qB-mbuZbGg7JvaxDVm1eEnhCtlKHETEPAtHPWwHGA/gviz/tq?tqx=out:json";
+let studentsData = {};
+
+async function loadData() {
+    const resultBox = document.getElementById("resultBox");
+    resultBox.innerHTML = `<p class="loading">Data load ho raha hai... thora wait karein</p>`;
+
+    try {
+        const response = await fetch(SHEET_URL);
+        const text = await response.text();
+        const json = JSON.parse(text.substring(47).slice(0, -2));
+        const rows = json.table.rows;
+        const cols = json.table.cols;
+
+        rows.forEach(row => {
+            const student = {};
+            cols.forEach((col, i) => {
+                student[col.label] = row.c[i]? row.c[i].v : "";
+            });
+            if (
